95bbdb5651c55ecb72af58d80f1c2bc615018535
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-05-05 07:52:26 +0000

    Deploying to gh-pages from @ Birdman1104/horses-ride-animation@0ba6c30eb6a175d376018bed57e447c2839ec769 🚀

--- assets/assetsNames/atlas.ts
+++
@@ -1,8 +0,0 @@
-export const atlases: { name: string; json: string; png: string }[] = [
-    { name: 'diamond_horse', json: 'assets/atlas/diamond_horse@1.png.json', png: 'assets/atlas/diamond_horse.png' },
-    { name: 'game', json: 'assets/atlas/game@1.png.json', png: 'assets/atlas/game.png' },
-    { name: 'hearts_horse', json: 'assets/atlas/hearts_horse@1.png.json', png: 'assets/atlas/hearts_horse.png' },
-    { name: 'horse_clubs', json: 'assets/atlas/horse_clubs@1.png.json', png: 'assets/atlas/horse_clubs.png' },
-    { name: 'horses', json: 'assets/atlas/horses@1.png.json', png: 'assets/atlas/horses.png' },
-    { name: 'spades_horse', json: 'assets/atlas/spades_horse@1.png.json', png: 'assets/atlas/spades_horse.png' },
-];
